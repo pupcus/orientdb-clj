9376fe7e8b4f71630668ec2f80f5dd98c8ee6061
Michael Pendergrass <mdpendergrass@gmail.com>
2017-02-17 21:48:16 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -10,8 +10,8 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[cheshire "5.7.0"]
-                 [com.orientechnologies/orientdb-client "2.2.16"]
-                 [com.orientechnologies/orientdb-core "2.2.16"]
+                 [com.orientechnologies/orientdb-client "2.2.17"]
+                 [com.orientechnologies/orientdb-core "2.2.17"]
                  [com.stuartsierra/component "0.3.2"]
                  [honeysql "0.8.2"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
@@ -22,7 +22,7 @@
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.slf4j/slf4j-log4j12 "1.7.5"]]}
-             :test {:dependencies [[kosmos/kosmos-orientdb-server "0.0.2"]]}}
+             :test {:dependencies [[kosmos/kosmos-orientdb-server "0.0.3"]]}}
 
   :deploy-repositories [["snapshots"
                          {:url "https://clojars.org/repo"
